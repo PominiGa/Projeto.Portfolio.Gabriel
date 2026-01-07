0e6a0e11b73a1916cf62ef5731cfa8d01f7a1136
PominiGa <gabriel.souza53@hotmail.com>
2026-01-06 21:12:08 -0300

    home atualizado

--- portfolio/src/pages/HomePage.tsx
+++ portfolio/src/pages/HomePage.tsx
@@ -6,15 +6,15 @@ import ContactForm from "../components/contact/ContactForm";
 
 
 export default function HomePage() {
-return (
-<div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 antialiased">
-<Header />
-<main id="main">
-<Hero />
-<AboutSection />
-<ContactForm />
-</main>
-<Footer />
-</div>
-);
+    return (
+        <div className="min-h-screen bg-white dark:bg-gray-900 text-gray-900 dark:text-gray-100 antialiased">
+            <Header />
+            <main id="main">
+                <Hero />
+                <AboutSection />
+                <ContactForm />
+            </main>
+            <Footer />
+        </div>
+    );
 }
